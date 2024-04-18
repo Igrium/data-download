1451d7661ad483e3a3562608b8fb3a5b92b31696
Sam54123 <thesam54123@gmail.com>
2024-04-18 13:07:20 -0700

    added url downloading

--- src/main/java/com/igrium/datadownload/DataCommandExtras.java
+++ src/main/java/com/igrium/datadownload/DataCommandExtras.java
@@ -9,19 +9,26 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.function.Function;
 
 import com.igrium.datadownload.filebin.FilebinApi;
 import com.igrium.datadownload.filebin.FilebinMeta;
-import com.igrium.datadownload.filebin.HttpException;
 import com.igrium.datadownload.filebin.FilebinMeta.FilebinFileMeta;
+import com.igrium.datadownload.filebin.HttpException;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 
 import net.minecraft.command.CommandRegistryAccess;
@@ -37,14 +44,13 @@ import net.minecraft.text.HoverEvent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.Texts;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.Util;
 
 public class DataCommandExtras {
 
     private static final SimpleCommandExceptionType UPLOAD_FAILED = new SimpleCommandExceptionType(Text.literal("Error exporting data. See console for details."));
-    
+    private static final DynamicCommandExceptionType INVALID_URL = new DynamicCommandExceptionType(s -> Text.literal(null));
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess,
             RegistrationEnvironment environment) {
         
@@ -68,12 +74,16 @@ public class DataCommandExtras {
                 objectType.addArgumentsToBuilder(literal("import"), builder -> builder.then(
                     literal("from").then(
                         literal("filebin").then(
-                            argument("binId", StringArgumentType.string()).executes(
+                            argument("binId", StringArgumentType.word()).executes(
                                 context -> importFilebin(context, objectType.getObject(context))
                             )
                         )
                     ).then(
-                        literal("url")
+                        literal("url").then(
+                            argument("url", StringArgumentType.greedyString()).executes(
+                                context -> importUrl(context, objectType.getObject(context))
+                            )
+                        )
                     )
                 ))
             );
@@ -94,33 +104,26 @@ public class DataCommandExtras {
         // }
     }
 
-    private static int importFilebin(CommandContext<ServerCommandSource> context, DataCommandObject object) throws CommandSyntaxException {
-        String binId = StringArgumentType.getString(context, "binId");
-        FilebinApi filebin = DataDownload.getInstance().getFilebin();
-
+    private static CompletableFuture<?> downloadNbt(CommandContext<ServerCommandSource> context, DataCommandObject object,
+            Function<FilebinApi, CompletableFuture<NbtCompound>> downloadFunction, Text notFoundText) {
+        
         context.getSource().sendFeedback(() -> Text.literal("Importing data..."), false);
-
-        filebin.getBinMeta(binId).thenApplyAsync(bin -> {
-
-            var file = getBestFile(bin);
-            try(InputStream in = new BufferedInputStream(filebin.download(bin.bin.id, file.filename))) {
-                return NbtIo.readCompound(new DataInputStream(in));
-
-            } catch (IOException e) {
-                throw new CompletionException(e);
-            }
-        }, Util.getIoWorkerExecutor()).whenCompleteAsync((nbt, e) -> {
-
+        
+        FilebinApi filebin = DataDownload.getInstance().getFilebin();
+        return downloadFunction.apply(filebin).whenCompleteAsync((nbt, e) -> {
             if (e != null) {
                 e = FilebinApi.decomposeCompletionException(e);
-                
                 if (e instanceof DownloadException) {
                     context.getSource().sendError(Text.literal(e.getMessage()));
-                } else if (e instanceof HttpException http && http.getStatusCode() == 404) {
-                    context.getSource().sendError(Text.literal("The supplied bin was not found."));
+
+                } else if (e instanceof HttpException http) {
+                    if (http.getStatusCode() == 404) {
+                        context.getSource().sendError(notFoundText);
+                    } else {
+                        context.getSource().sendError(Text.literal("The server returned status code " + http.getStatusCode()));
+                    }
                 } else {
                     context.getSource().sendError(Text.literal("An error occured importing the file: " + e.getMessage()));
-                    DataDownload.LOGGER.error("Error importing nbt file.", e);
                 }
                 return;
             }
@@ -128,12 +131,57 @@ public class DataCommandExtras {
             try {
                 object.setNbt(nbt);
             } catch (CommandSyntaxException e1) {
-                context.getSource().sendError(Texts.toText(e1.getRawMessage()));
+                context.getSource().sendError(Text.of(e1.getRawMessage()));
+                DataDownload.LOGGER.error("Error importing NBT file.", e);
             }
 
-            context.getSource().sendFeedback(() -> Text.literal("Imported nbt data."), true);
-
+            context.getSource().sendFeedback(() -> Text.literal("Successfully imported data!"), false);
         }, context.getSource().getServer());
+    }
+
+    private static int importFilebin(CommandContext<ServerCommandSource> context, DataCommandObject object) throws CommandSyntaxException {
+        String binId = StringArgumentType.getString(context, "binId");
+
+        downloadNbt(context, object, filebin -> filebin.getBinMeta(binId).thenApply(bin -> {
+            var file = getBestFile(bin);
+
+            try(InputStream in = new BufferedInputStream(filebin.download(bin.bin.id, file.filename))) {
+                return NbtIo.readCompound(new DataInputStream(in));
+            } catch (IOException e) {
+                throw new CompletionException(e);
+            }
+
+        }), Text.literal("The supplied bin was not found."));
+
+        return 1;
+    }
+
+    private static int importUrl(CommandContext<ServerCommandSource> context, DataCommandObject object) throws CommandSyntaxException {
+        String urlString = StringArgumentType.getString(context, "url");
+        URI url;
+        try {
+            url = new URI(urlString);
+        } catch (URISyntaxException e) {
+           throw INVALID_URL.create(e.getMessage());
+        }
+
+        var request = HttpRequest.newBuilder(url)
+            .GET()
+            .build();
+
+        downloadNbt(context, object,
+                filebin -> filebin.getClient().sendAsync(request, BodyHandlers.ofInputStream()).thenApply(res -> {
+                    if (res.statusCode() >= 400) {
+                        throw new CompletionException(new HttpException(res.statusCode()));
+                    }
+
+                    try(InputStream in = new BufferedInputStream(res.body())) {
+                        return NbtIo.readCompound(new DataInputStream(in));
+                    } catch (IOException e) {
+                        throw new CompletionException(e);
+                    }
+                    
+                }), Text.literal("404 not found."));
 
         return 1;
     }
